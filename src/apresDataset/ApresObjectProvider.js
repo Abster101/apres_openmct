01b6e31214852d005149f79a9b37cd2fdcfd43fa
Deep Tailor <deep.j.tailor@nasa.gov>
2021-06-21 21:08:31 -0700

    wip new providers

--- src/apresDataset/ApresObjectProvider.js
+++ src/apresDataset/ApresObjectProvider.js
@@ -1,39 +1,95 @@
+import { activityTypes } from '../../config/action_types';
 
 export default class ApresObjectProvider {
-    constructor(dataset) {
-        this.dataset = dataset;
-    }
-    get(identifier) {
-        const activityProcType = identifier.key.split('-')[1];
-        const modelObject = this.dataset.actionTypes[activityProcType];
+    constructor(datasetCache) {
+        this.datasetCache = datasetCache;
 
-        return Promise.resolve({
-            identifier,
-            name: activityProcType,
-            cssClass: 'icon-activity',
-            type: `apres.activity.${activityProcType}`,
-            configuration: {
-                colorHex: modelObject.colorHex,
-                timelineLegend: modelObject.timelineLegend,
-                startTime: 0,
-                parameters: {
-                    drillDur: {
-                        duration: 3600,
-                        type: "integer",
-                        unit: 'seconds'
-                    }
+        this.getActionDefinitions = this.getActionDefinitions.bind(this);
+        this.getActionTypesComposition = this.getActionTypesComposition.bind(this);
+        this.getAction = this.getAction.bind(this);
+
+        this.actionDefinitions = this.getActionDefinitions();
+    }
+
+    getActionDefinitions() {
+        const definitions = {};
+
+        activityTypes.modelConfig.forEach(action => {
+            definitions[action.actProcType] = action;
+        });
+
+        return definitions;
+    }
+
+    getActionTypesComposition(identifier) {
+        const composition = [];
+
+        activityTypes.modelConfig.forEach(action => {
+            composition.push(`apres:actions.type.${action.actProcType}.${identifier.key}`);
+        });
+
+        return composition;
+    }
+
+    getAction(identifier) {
+        const keyArray = identifier.key.split('.');
+
+        if (keyArray[1] === 'source') {
+            return Promise.resolve({
+                identifier,
+                name: 'Actions',
+                type: 'apres.action.source',
+                location: keyArray[2],
+                configuration: {
+                    actionTypes: this.actionDefinitions
                 },
-                duration: 3600,
-                objectStyles: {
-                    staticStyle: {
-                        style: {
-                            backgroundColor: modelObject.colorHex,
-                            border: `1px solid ${modelObject.colorHex}`,
-                            color: '#aaaaaa'
+                composition: this.getActionTypesComposition(identifier)
+            });
+        } else {
+            const proctype = keyArray[2];
+            const modelObject = this.actionDefinitions[proctype];
+            const location = `${identifier.namespace}:${keyArray.slice(3).join('.')}`;
+
+            return Promise.resolve({
+                identifier,
+                name: proctype,
+                cssClass: 'icon-activity',
+                type: `apres.activity.${proctype}`,
+                location: location,
+                configuration: {
+                    colorHex: modelObject.colorHex,
+                    timelineLegend: modelObject.timelineLegend,
+                    startTime: 0,
+                    parameters: {
+                        drillDur: {
+                            duration: 3600,
+                            type: "integer",
+                            unit: 'seconds'
+                        }
+                    },
+                    duration: 3600,
+                    objectStyles: {
+                        staticStyle: {
+                            style: {
+                                backgroundColor: modelObject.colorHex,
+                                border: `1px solid ${modelObject.colorHex}`,
+                                color: '#aaaaaa'
+                            }
                         }
                     }
                 }
-            }
-        });
+            });
+        }
+    }
+    get(identifier) {
+        const keyArray = identifier.key.split('.');
+
+        if (keyArray[0] === 'actions') {
+            return this.getAction(identifier);
+        } else if (keyArray[0] === 'stateChronicles') {
+            return this.getStateChronicle(identifier);
+        }
+
+        return Promise.reject();
     }
 }
