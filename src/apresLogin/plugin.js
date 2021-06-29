a0d447e51e64b4dea1b430690b9af76a2754742d
Abiola Akanni <abiola.o.akanni@nasa.gov>
2021-06-29 11:32:10 -0700

    adding to login

--- src/apresLogin/plugin.js
+++ src/apresLogin/plugin.js
@@ -4,8 +4,6 @@
  */
 export default function () {
     return function install (openmct) {
-
-<<<<<<< HEAD
         let typeDef = {
             name: 'Login Page',
             cssClass: 'icon-plus',
@@ -36,37 +34,5 @@ export default function () {
         };
 
         openmct.types.addType(activityKey, typeDef);
-=======
-                let typeDef = {
-                    name: 'Login Page',
-                    cssClass: 'icon-plus',
-                    creatable: true,
-                    initialize: function (domainObject) {
-                        domainObject.configuration = configuration;
-                    },
-                    form: [
-                        {
-                            name: "Login",
-                            control: "textfield",
-                            cssClass: "l-input-sm l-numeric",
-                            required: true,
-                            property: [
-                                "configuration",
-                            ]
-                        },
-                        {
-                            name: "Password",
-                            control: "textfield",
-                            cssClass: "l-input-sm l-numeric",
-                            required: true,
-                            property: [
-                                "configuration",
-                            ]
-                        }
-                    ]
-                };
-
-                openmct.types.addType(activityKey, typeDef);
->>>>>>> 267327c534f02a19378004f48af6ced1c021fd60
     }
 }
