a0ce64f94a5662a58301592311e365c473fbffb6
Abiola Akanni <abiola.o.akanni@nasa.gov>
2021-06-29 11:05:26 -0700

    skeleton with DataSet

---
+++ src/apresLogin/plugin.js
@@ -0,0 +1,39 @@
+/**
+ * This plugin allows the USer to login with a username and password before entering the APRES system
+ * @returns {(function(*): void)|*}
+ */
+export default function () {
+    return function install (openmct) {
+
+        let typeDef = {
+            name: 'Login Page',
+            cssClass: 'icon-plus',
+            creatable: true,
+            initialize: function (domainObject) {
+                domainObject.configuration = configuration;
+            },
+            form: [
+                {
+                    name: "Login",
+                    control: "textfield",
+                    cssClass: "l-input-sm l-numeric",
+                    required: true,
+                    property: [
+                        "configuration",
+                    ]
+                },
+                {
+                    name: "Password",
+                    control: "textfield",
+                    cssClass: "l-input-sm l-numeric",
+                    required: true,
+                    property: [
+                        "configuration",
+                    ]
+                }
+            ]
+        };
+
+        openmct.types.addType(activityKey, typeDef);
+    }
+}
