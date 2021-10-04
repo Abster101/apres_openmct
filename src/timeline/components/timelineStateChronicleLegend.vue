f89c8cb8a78e380d8e0d427d02e8e62708c8d3f2
Alize Nguyen <alizenguyen@gmail.com>
2021-10-04 10:18:38 -0500

    TimelineStateChronicle File added.

---
+++ src/timeline/components/timelineStateChronicleLegend.vue
@@ -0,0 +1,120 @@
+<template>
+<ul :style="legendStyle" v-if="inBoundsEpisodes.length > 0">
+    <p :style="titleStyle">{{displayedValue}}</p>
+   <timeline-state-chronicle
+        v-for="(episode, index) in inBoundsEpisodes"
+        :key="'episode-' + episode.time"
+        :domainObject="episode"
+        :parentDomainObject="parentDomainObject"
+        :index="index"
+        :isEditing="isEditing"
+        :startBounds="startBounds"
+        :endBounds="endBounds"
+        :pixelMultiplier="pixelMultiplier"
+        :formatter="formatter"
+        :errors="errors"
+        :violationClicked="violationClicked"
+        @onEpisodeHover="changeDisplayValue" 
+    /> 
+</ul>
+</template>
+
+<script>
+import TimelineStateChronicle from './timelineStateChronicle.vue';
+
+const ACTIVITY_HEIGHT = 44;
+
+export default {
+    inject: ['openmct'],
+    components: {
+        TimelineStateChronicle
+    },
+    props: {
+        chronicle: {
+            type: Object,
+            required: true,
+            default() {
+                return {};
+            }
+        },
+        parentDomainObject: {
+            type: Object,
+            required: true,
+            default() {
+                return {}
+            }
+        },
+        title: {
+            type: String
+        },
+        index: {
+            type: Number
+        },
+        isEditing: {
+            type: Boolean
+        },
+        startBounds: {
+            type: Number
+        },
+        endBounds: {
+            type: Number
+        },
+        pixelMultiplier: {
+            type: Number
+        },
+        formatter: {
+            type: Object
+        },
+        errors: {
+            type: Array
+        },
+        violationClicked: {
+            type: Boolean
+        },
+    },
+    data() {
+        return {
+            displayedValue: "",
+        }
+    },
+    mounted() {
+        const lastEpisode = this.chronicle.episodes[this.chronicle.episodes.length-1];
+        this.displayedValue = lastEpisode.value;
+    },
+    computed: {
+        legendStyle() {
+            return {
+                'position': 'relative',
+                'max-height': `${ACTIVITY_HEIGHT * 1}px`,
+                'min-height': `${ACTIVITY_HEIGHT * 1}px`,
+                'border-top': '2px solid #6c6c6c',
+                'border-bottom': '2px solid #6c6c6c',
+                'margin-top': '10px',
+				'margin-bottom': '10px',
+            }
+        },
+        titleStyle() {
+            return {
+                'position': 'absolute',
+                'margin-left': '10px',
+                'z-index': '5',
+            }
+        },
+        inBoundsEpisodes() {
+            const filteredEpisodes = this.chronicle.episodes.filter(episode => {
+                return (
+                    this.formatter.parse(episode.time) <= this.endBounds
+                );
+            });
+
+            const test = [filteredEpisodes[filteredEpisodes.length-1]]
+            return filteredEpisodes;
+        }
+    },
+    methods: {
+        changeDisplayValue(value) {
+            this.displayedValue = value;
+        },
+    },
+}
+</script>
