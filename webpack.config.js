eb364763da1d4e0fd71c0a23f2d901631ccccb4c
Joe Pea <joe@trusktr.io>
2021-10-27 02:17:46 +0300

    pin versions to avoid what appears to be a breaking in-range break in webpack-cli and interaction with webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -92,7 +92,7 @@ module.exports = {
         ]
     },
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        static: path.join(__dirname, 'dist'),
         compress: true,
         port: 9000,
     },
