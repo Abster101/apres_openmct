b9e856938060cc84abf0d46fd91c06c5a4db9e7b
heslami200 <57686617+heslami200@users.noreply.github.com>
2022-01-06 09:13:30 -0800

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import StartScreen from './startScreen/StartScreen.vue';
-
-function initializeApp() {
-    const element = document.querySelector('#app');
-    createApp(StartScreen).mount(element)
-}
-
-document.addEventListener('DOMContentLoaded', initializeApp);
